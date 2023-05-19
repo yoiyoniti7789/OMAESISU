2bd428b05d5c5196a5e7b93c71ec1d5068da395f
Niti.yoiyo@gmail.com <Niti.yoiyo@gmail.com>
2023-05-19 14:59:32 +0900

    君いい肉だね

--- main.cpp
+++ main.cpp
@@ -1,24 +1,7 @@
 #include <stdio.h>
-#include <list>
-#include <iostream>
-using namespace std;
 
 int main() {
 
-	list<int> lst{1,2,3};
-	auto itr = lst.begin();
-	cout << *itr << endl;
-	*itr = 9;
-	cout << *itr << endl;
-	itr++;
-	*itr = 10;
-	cout << *itr << endl;
-	lst.push_front(114);
-	lst.push_back(514);
-	for (auto itr = lst.begin();itr != lst.end();++itr) 
-	{
-		cout << *itr << "\n";
-	}
-
+	
 	return 0;
 }
